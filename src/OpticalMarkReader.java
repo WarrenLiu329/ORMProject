7471865d7cf265539eecc1ea939f395e248c4fa6
L.B. Warren Liu <warrenliu403@gmail.com>
2017-03-10 09:54:42 -0800

    debug using print statements

--- src/OpticalMarkReader.java
+++ src/OpticalMarkReader.java
@@ -21,23 +21,28 @@ public class OpticalMarkReader {
 		// calculate a starting point and predetermine distances between bubbles
 		// Location start = calcStartingPoint(image);
 		Location start = new Location(110, 462);
-	//	AnswerSheet sheet = new AnswerSheet();
+		// AnswerSheet sheet = new AnswerSheet();
 
 		int[][] pixels = getTwoDimArray(image);
 		int numBubbles = 5;
 		int bubbleWidth = 200 / numBubbles;
 		int bubbleHeight = 37;
 		int distBetweenCol = 83;
-		int stringLength = (image.width - start.getX())/200;
-		int stringHeight = image.height-start.getY();
 
-		String[][] answers = new String[stringLength][];
+		int row = 0;
+		int col = 0;
+		String[][] answers = new String[7][100];
 		for (int j = start.getY(); j < image.width; j += bubbleWidth + distBetweenCol) {
 			for (int i = start.getX(); i < image.height; i += bubbleHeight) {
-				//System.out.println("(" + i + ", "+ j +  ")");
+				// System.out.println("(" + i + ", "+ j + ")");
 				int bubbleIndex = determineBubble(i, j, bubbleWidth, bubbleHeight, numBubbles, pixels);
-				//answers.add(determineAnswer(bubbleIndex));
+				System.out.println("row: " + row + " col: " + col);
+				System.out.println("i: " + i + " j: " + j);
+				answers[row][col] = determineAnswer(bubbleIndex);
+				System.out.println(answers[row][col]);
+				col++;
 			}
+			row++;
 		}
 		return null;
 	}
@@ -49,11 +54,10 @@ public class OpticalMarkReader {
 	 * @param image
 	 * @return the grey-color value between 0 and 255
 	 */
-	public int getPixelAt(int row, int col, PImage image) {
+	public static int getPixelAt(int row, int col, PImage image) {
 		image.loadPixels();
 		int index = row * image.width + col;
-		int color = image.pixels[index];
-		return color & 255;
+		return image.pixels[index] & 255;
 	}
 
 	public int[][] getTwoDimArray(PImage image) {
@@ -67,34 +71,6 @@ public class OpticalMarkReader {
 		return pixels;
 	}
 
-	// public Location calcStartingPoint(PImage image) {
-
-	// }
-
-	// public int determineBubble(int r, int c, int width, int height, int
-	// numBubbles, int[][] pixels) {
-	// int darkestIndex = 0;
-	// int darkestVal = 255;
-	// int index = 0;
-	// for (int col = c; col < pixels[0].length - width; col += pixels[0].length
-	// / numBubbles) {
-	// int sum = 0;
-	// for (int i = r; i < (pixels.length / numBubbles) * r; i++) {
-	// for (int j = col; j < (pixels[0].length / numBubbles) * col; j++) {
-	// sum += pixels[i][j];
-	//
-	// }
-	// if (sum < darkestVal) {
-	// darkestIndex = index;
-	// darkestVal = sum;
-	// }
-	// }
-	//
-	// index++;
-	// }
-	//
-	// return darkestIndex;
-	// }
 
 	public int determineBubble(int r, int c, int width, int height, int numBubbles, int[][] pixels) {
 		int boxWidth = width / numBubbles;
@@ -102,7 +78,7 @@ public class OpticalMarkReader {
 		int maxBubble = 0;
 		for (int i = 0; i < numBubbles; i++) {
 			int value = getSumValue(r, c + i * boxWidth, width, height, pixels);
-			//System.out.println(i + ": " + value);
+			// System.out.println(i + ": " + value);
 			if (value < darkestVal) {
 				darkestVal = value;
 				maxBubble = i;
@@ -113,19 +89,23 @@ public class OpticalMarkReader {
 	}
 
 	public int getSumValue(int r, int c, int width, int height, int[][] pixels) {
-		int sum = 0;
-	//	System.out.println("length: " + pixels.length + " height: " + pixels[0].length);
-	//	System.out.println("x: " + r + " y: " + c);
+
+		int count = 0;
+		// System.out.println("length: " + pixels.length + " height: " +
+		// pixels[0].length);
+		// System.out.println("x: " + r + " y: " + c);
 		for (int i = r; i < height; i++) {
 			for (int j = c; j < width; j++) {
 				System.out.println(pixels[i][j]);
-				sum += pixels[i][j];
-				
-				//System.out.println(sum);
+				if (pixels[i][j] == 0) {
+					count++;
+				}
+
+				// System.out.println(sum);
 			}
 		}
 
-		return sum ;
+		return count;
 	}
 
 	public String determineAnswer(int i) {
