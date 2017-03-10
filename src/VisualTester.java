c68a4f61de55fa61b05bc11cf955c9b6d318e7a8
L.B. Warren Liu <warrenliu403@gmail.com>
2017-03-10 08:54:30 -0800

    debug using visula tester and display the pixel value

--- src/VisualTester.java
+++ src/VisualTester.java
@@ -33,31 +33,32 @@ public class VisualTester extends PApplet {
 			current_image = images.get(currentImageIndex);
 			image(current_image, 0, 0); // display image i
 			textSize(50);
-			int myColor = color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+			int myColor = color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
 			fill(0);
-		//	rect(3, 259, 40, 40);
+			// rect(3, 259, 40, 40);
 			text(mouseX + " " + mouseY, 30, 30);
 			noFill();
-			
+
 			stroke(0);
 			strokeWeight(2);
-			int width = 200;
-			int height = 37;
-			int topCornerX = 114;
-			int topCornerY = 462;
-			int xIncrement = width + 83;
-			int section = width / 5;
-			for (int i = 0; i < 4; i++) {
-				for (int j = 0; j < 12; j++) {
-					int rectY = topCornerY + (j * height);
-					rect(topCornerX + (xIncrement * i), rectY, width, height);
-					for (int s = 1; s < 5; s++) {
-						line(topCornerX + (section * s) + (xIncrement * i), rectY,
-								topCornerX + (section * s) + (xIncrement * i), rectY + height);
-					}
-				}
-			}
-
+			// int width = 200;
+			// int height = 37;
+			// int topCornerX = 114;
+			// int topCornerY = 462;
+			// int xIncrement = width + 83;
+			// int section = width / 5;
+			// for (int i = 0; i < 4; i++) {
+			// for (int j = 0; j < 12; j++) {
+			// int rectY = topCornerY + (j * height);
+			// rect(topCornerX + (xIncrement * i), rectY, width, height);
+			// for (int s = 1; s < 5; s++) {
+			// line(topCornerX + (section * s) + (xIncrement * i), rectY,
+			// topCornerX + (section * s) + (xIncrement * i), rectY + height);
+			// }
+			// }
+			// }
+			int color = OpticalMarkReader.getPixelAt(mouseX, mouseY, current_image);
+			text(color, mouseX, mouseY + 40);
 		}
 	}
 
